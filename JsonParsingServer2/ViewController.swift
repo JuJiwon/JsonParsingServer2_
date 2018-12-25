647bf1db3bc7fa6f7400789b3134d3ac67546e04
iWon Ju J <jiwonju@JiWonui-iMac.local>
2018-12-25 22:58:48 +0900

    Initial Commit

---
+++ JsonParsingServer2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JsonParsingServer2
+//
+//  Created by JiWon Ju on 25/12/2018.
+//  Copyright © 2018 JiWon Ju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ JsonParsingServer2Tests/JsonParsingServer2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  JsonParsingServer2Tests.swift
+//  JsonParsingServer2Tests
+//
+//  Created by JiWon Ju on 25/12/2018.
+//  Copyright © 2018 JiWon Ju. All rights reserved.
+//
+
+import XCTest
+@testable import JsonParsingServer2
+
+class JsonParsingServer2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
